beaaa03e845c61b43536998168208921568a1c11
Molly Trombley-McCann <molly.kayline@gmail.com>
2024-01-21 18:03:32 -0800

    Binary tree and search tree

--- binary-trees/binary_search_tree.rb
+++ binary-trees/binary_search_tree.rb
@@ -10,4 +10,83 @@ require_relative 'binary_tree'
 #   empty?              O(1) time
 
 class BinarySearchTree < BinaryTree
+  def include?(value)
+    puts "Current evaluating #{@value}"
+    if @value == value
+      puts "Match!"
+      return true
+    elsif @value > value && !@left.nil?
+      puts "Too big"
+      @left.include?(value)
+    elsif !@right.nil?
+      puts "Too small"
+      @right.include?(value)
+    else
+      false
+    end
+  end
+
+  def insert(newLeaf)
+    insertionNode = nil
+    currentNode = self
+
+    while currentNode != EMPTY_TREE
+      insertionNode = currentNode
+      if newLeaf.value < currentNode.value
+        currentNode = currentNode.left
+      else
+        currentNode = currentNode.right
+      end
+    end
+    if insertionNode.empty?
+      self.value = newLeaf.value
+    elsif newLeaf.value < insertionNode.value
+      insertionNode.left = newLeaf
+    else
+      insertionNode.right = newLeaf
+    end
+  end
+
+  def remove(value_to_remove)
+    if @value == value_to_remove
+      self.remove_node
+    elsif value_to_remove < @value
+      @left.remove(value_to_remove)
+    else
+      @right.remove(value_to_remove)
+    end
+  end
+
+  def remove_node
+    if @left.nil? && @right.nil?
+      return EMPTY_TREE
+    elsif @left.nil?
+      @value = @right.value
+      @left = @right.left
+      @right = @right.right
+    elsif @right.nil?
+      @value = @left.value
+      @left = @left.left
+      @right = @left.right
+    elsif @right.left.nil?
+      @value = @right.value
+      @right = @right.right
+    else
+      @value = @right.left.value
+      @right.left.value = @right.left.right.value
+      @right.left.right = @right.left.right.right
+      @right.left.left = @right.left.right.left
+    end
+    self
+  end
 end
+
+tree = BinarySearchTree.new(3, BinarySearchTree.new(1), BinarySearchTree.new(5))
+tree.insert(BinarySearchTree.new(10))
+tree.insert(BinarySearchTree.new(7))
+tree.insert(BinarySearchTree.new(12))
+puts "*" * 70
+puts tree.to_string
+tree.remove(3)
+puts "*" * 70
+puts tree.to_string
--- binary-trees/binary_tree.rb
+++ binary-trees/binary_tree.rb
@@ -1,4 +1,5 @@
 # Implement a basic (recursive) Binary Tree
+EMPTY_TREE = nil
 
 def BinaryTree(value)
   case value
@@ -15,21 +16,112 @@ end
 class BinaryTree
   attr_accessor :value, :left, :right
 
-  def initialize(value = nil)
+  def initialize(value = nil, left = EMPTY_TREE, right = EMPTY_TREE)
+    @value = value
+    @left = left
+    @right = right
   end
 
   def each(&block)
+    if self.empty?
+      nil
+    else
+      block.call(@value, @left, @right)
+      unless @left.nil?
+        @left.each(&block)
+      end
+      unless @right.nil?
+        @right.each(&block)
+      end
+    end
   end
 
   # Implement pre-order traversal of the tree
   def pre_order(&block)
+    if self.empty?
+      nil
+    else
+      block.call(@value, @left, @right)
+      unless @left.nil?
+        @left.pre_order(&block)
+      end
+      unless right.nil?
+        @right.pre_order(&block)
+      end
+    end
   end
 
   # Implement in-order traversal of the tree
   def in_order(&block)
+    if self.empty?
+      nil
+    else
+      unless @left.nil?
+        @left.in_order(&block)
+      end
+      block.call(@value, @left, @right)
+      unless @right.nil?
+        @right.in_order(&block)
+      end
+    end
   end
 
   # Implement post-order traversal of the tree
   def post_order(&block)
+    if self.empty?
+      nil
+    else
+      unless @right.nil?
+        @right.post_order(&block)
+      end
+      unless @left.nil?
+        @left.post_order(&block)
+      end
+      block.call(@value, @left, @right)
+    end
+  end
+
+  def empty?
+    self == EMPTY_TREE
+  end
+
+  def to_string(padding = "", pointer = "")
+    if self.nil?
+      return ""
+    end
+
+    result = "#{padding}#{pointer}#{@value}\n"
+    right_pointer = " └── "
+    left_pointer = @right.nil? ? right_pointer : " ├── "
+
+    unless @left.nil?
+      result += @left.subtree_to_string(left_pointer, padding, !@right.nil?)
+    end
+    unless @right.nil?
+      result += @right.subtree_to_string(right_pointer, padding, false)
+    end
+
+    result
+  end
+
+  def subtree_to_string(pointer = "", padding = "", has_right_sibling = false)
+    if self.nil?
+      return ""
+    end
+
+    result = "#{padding}#{pointer}#{@value}\n"
+    right_pointer = " └── "
+    left_pointer = @right.nil? ? right_pointer : " ├── "
+
+    new_padding = has_right_sibling ? " │  " : "    "
+
+    unless @left.nil?
+      result += @left.subtree_to_string(left_pointer, padding + new_padding, !@right.nil?)
+    end
+    unless @right.nil?
+      result += @right.subtree_to_string(right_pointer, padding + new_padding, false)
+    end
+
+    result
   end
 end
