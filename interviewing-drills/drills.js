3b81fc2dbc1dbf13d5ede232629638665d006be4
Jesse Farmer <jesse@20bits.com>
2024-05-06 11:02:28 -0700

    Add comments to drill functions

--- interviewing-drills/drills.js
+++ interviewing-drills/drills.js
@@ -15,34 +15,82 @@ class BinaryTree {
   }
 }
 
-function listForEachRecursive(head, callback = doNothing) {
 
-}
-
+/**
+ * Traverse a linked list, iteratively
+ */
 function listForEachIterative(head, callback = doNothing) {
+  let current = head;
 
+  while (current !== null) {
+    callback(current);
+    current = current.next;
+  }
 }
 
+/**
+ * Traverse a linked list, recursively
+ */
+function listForEachRecursive(head, callback = doNothing) {
+  if (head === null) {
+    return;
+  }
+
+  callback(head);
+
+  listForEachRecursive(head.next, callback);
+}
+
+/**
+ * Traverse a binary tree, recursively and depth-first
+ *
+ * The callback can be invoked in any order, but only once per node
+ */
 function binaryTreeDFSRecursive(root, callback = doNothing) {
 
 }
 
+/**
+ * Traverse a binary tree, iteratively and depth-first
+ *
+ * The callback can be invoked in any order, but only once per node
+ */
 function binaryTreeDFSIterative(root, callback = doNothing) {
 
 }
 
+/**
+ * Traverse a binary tree, iteratively and breadth-first
+ *
+ * The callback can be invoked in any order, but only once per node
+ */
 function binaryTreeBFSIterative(root, callback = doNothing) {
 
 }
 
+/**
+ * Traverse a graph given a starting node, recursively and depth-first
+ *
+ * The callback can be invoked in any order, but only once per node
+ */
 function graphDFSRecursive(graph, start, callback = doNothing) {
 
 }
 
+/**
+ * Traverse a graph given a starting node, iteratively and depth-first
+ *
+ * The callback can be invoked in any order, but only once per node
+ */
 function graphDFSIterative(graph, start, callback = doNothing) {
 
 }
 
+/**
+ * Traverse a graph given a starting node, iteratively and breadth-first
+ *
+ * The callback can be invoked in any order, but only once per node
+ */
 function graphBFSIterative(graph, start, callback = doNothing) {
 
 }
