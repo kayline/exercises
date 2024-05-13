c4ffdaf531e834efa9038882fd579fd650724ab5
Molly Trombley-McCann <molly.kayline@gmail.com>
2024-05-12 22:27:26 -0700

    iteration drills

--- interviewing-drills/drills.js
+++ interviewing-drills/drills.js
@@ -23,7 +23,7 @@ function listForEachIterative(head, callback = doNothing) {
   let current = head;
 
   while (current !== null) {
-    callback(current);
+    callback();
     current = current.next;
   }
 }
@@ -36,7 +36,7 @@ function listForEachRecursive(head, callback = doNothing) {
     return;
   }
 
-  callback(head);
+  callback();
 
   listForEachRecursive(head.next, callback);
 }
@@ -47,7 +47,14 @@ function listForEachRecursive(head, callback = doNothing) {
  * The callback can be invoked in any order, but only once per node
  */
 function binaryTreeDFSRecursive(root, callback = doNothing) {
+  if (root === null) {
+    return;
+  }
 
+  binaryTreeDFSRecursive(root.left, callback);
+  binaryTreeDFSRecursive(root.right, callback);
+
+  callback(root.value);
 }
 
 /**
@@ -56,6 +63,18 @@ function binaryTreeDFSRecursive(root, callback = doNothing) {
  * The callback can be invoked in any order, but only once per node
  */
 function binaryTreeDFSIterative(root, callback = doNothing) {
+  queue = [root];
+
+  while (queue.length > 0) {
+    node = queue.pop();
+    callback(node.value);   
+    if (node.right) {
+      queue.push(node.right);
+    }
+    if (node.left) {
+      queue.push(node.left);
+    }
+  }
 
 }
 
@@ -65,7 +84,18 @@ function binaryTreeDFSIterative(root, callback = doNothing) {
  * The callback can be invoked in any order, but only once per node
  */
 function binaryTreeBFSIterative(root, callback = doNothing) {
+  queue = [root];
 
+  while (queue.length > 0) {
+    node = queue.shift();
+    callback(node.value);
+    if (node.left) {
+      queue.push(node.left);
+    }
+    if (node.right) {
+      queue.push(node.right);
+    }
+  }
 }
 
 /**
@@ -73,8 +103,20 @@ function binaryTreeBFSIterative(root, callback = doNothing) {
  *
  * The callback can be invoked in any order, but only once per node
  */
-function graphDFSRecursive(graph, start, callback = doNothing) {
+function graphDFSRecursive(graph, start, visited, callback = doNothing) {
+  if(!visited.has(start)) {
+    callback(start);
+    visited.add(start);
 
+    let childNodes = graph[start];  
+    if(childNodes.length === 0) {
+      return
+    }
+    
+    for (let childNode of childNodes) {
+      graphDFSRecursive(graph, childNode, visited, callback);
+    }
+  }  
 }
 
 /**
@@ -83,7 +125,19 @@ function graphDFSRecursive(graph, start, callback = doNothing) {
  * The callback can be invoked in any order, but only once per node
  */
 function graphDFSIterative(graph, start, callback = doNothing) {
+  let queue = [start];
+  let visited = new Set();
 
+  while(queue.length > 0) {
+    node = queue.pop();
+    if(!visited.has(node)) {
+      visited.add(node);
+      callback(node);
+      for (let childNode of graph[node]) {
+        queue.push(childNode);
+      }
+    }   
+  }
 }
 
 /**
@@ -92,5 +146,43 @@ function graphDFSIterative(graph, start, callback = doNothing) {
  * The callback can be invoked in any order, but only once per node
  */
 function graphBFSIterative(graph, start, callback = doNothing) {
+  let queue = [start];
+  let visited = new Set();
 
+  while(queue.length > 0) {
+    node = queue.shift();
+    if(!visited.has(node)) {
+      visited.add(node);
+      callback(node);
+      for (childNode of graph[node]) {
+        queue.push(childNode);
+      }
+    }
+  }
 }
+
+function logValue(value) {
+  console.log(value);
+}
+
+let tree = new BinaryTree(25, new BinaryTree(31, new BinaryTree(33), new BinaryTree(27)), new BinaryTree(12, new BinaryTree(14), new BinaryTree(8)));
+let graph = {
+  B: [],
+  C: [ 'D' ],
+  D: [ 'B', 'E' ],
+  E: [],
+  A: [ 'B', 'C' ],
+  F: [ 'C' ]
+};
+console.log("DFS Recursive");
+binaryTreeDFSRecursive(tree, logValue);
+console.log("DFS Iterative");
+binaryTreeDFSIterative(tree, logValue);
+console.log("BFS Iterative");
+binaryTreeBFSIterative(tree, logValue);
+console.log("DFS Recursive");
+graphDFSRecursive(graph, 'A', new Set(), logValue);
+console.log("DFS Iterative");
+graphDFSIterative(graph, 'A', logValue);
+console.log("BFS Iterative");
+graphBFSIterative(graph, 'A', logValue);
